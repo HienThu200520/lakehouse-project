5e16279a7715b161d734e58fb97f2d98a118b689
HienThu200520 <84374871+HienThu200520@users.noreply.github.com>
2023-07-01 22:54:47 +0700

    Create accelerometer_landing_to_trusted.py

---
+++ scripts/accelerometer_landing_to_trusted.py
@@ -0,0 +1,23 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Define your transformation logic here
+# Example: Read data from accelerometer_landing table, filter records based on shareWithResearchAsOfDate, and store in accelerometer_trusted table
+
+datasource = glueContext.create_dynamic_frame.from_catalog(database = "my_database", table_name = "accelerometer_landing")
+filtered_data = Filter.apply(frame = datasource, f = lambda x: x["shareWithResearchAsOfDate"] is not None)
+
+glueContext.write_dynamic_frame.from_catalog(frame = filtered_data, database = "my_database", table_name = "accelerometer_trusted")
+
+job.commit()
