b84ef29bda75cdd64ed7225886a7139561ff67ed
HienThu200520 <84374871+HienThu200520@users.noreply.github.com>
2023-07-02 09:19:33 +0700

    Update customer_landing.sql

--- sql/customer_landing.sql
+++ sql/customer_landing.sql
@@ -1,11 +1,18 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS customer_landing (
-  -- Define the schema fields and data types based on your JSON structure
-  -- Example:
-  serialnumber STRING,
+  customer_id STRING,
   name STRING,
   email STRING,
   shareWithResearchAsOfDate DATE
 )
 ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
-LOCATION 's3://my_bucket/customer_landing/'
-;
+WITH SERDEPROPERTIES (
+  'serialization.format' = '1'
+)
+LOCATION 's3://my-bucket/customer_landing/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
+-- Filter out rows with blank shareWithResearchAsOfDate
+CREATE OR REPLACE VIEW customer_landing_filtered AS
+SELECT *
+FROM customer_landing
+WHERE shareWithResearchAsOfDate IS NOT NULL;
